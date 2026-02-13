0bbe84ba89dac620413893533457cf0e67a8644e
skmkqw <tkorsakov77@gmail.com>
2026-02-13 21:14:28 +0100

    create Platform model

---
+++ services/platform/PlatformService/Models/Platform.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
+
+public class Platform
+{
+  [Key] public int Id { get; set; }
+
+  [Required] public string Name { get; set; } = string.Empty;
+
+  [Required] public string Publisher { get; set; } = string.Empty;
+}
