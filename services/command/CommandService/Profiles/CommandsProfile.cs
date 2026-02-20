1ccbde73f14051e8629a2c0db8d930b10ca373a5
skmkqw <tkorsakov77@gmail.com>
2026-02-20 22:23:08 +0100

    add mapping profiles

---
+++ services/command/CommandService/Profiles/CommandsProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using CommandService.DTOs;
+using CommandService.Models;
+
+namespace CommandService.Profiles;
+
+public class CommandsProfile : Profile
+{
+    public CommandsProfile()
+    {
+        CreateMap<Platform, PlatformReadDto>();
+        CreateMap<CommandCreateDto, Command>();
+        CreateMap<Command, CommandReadDto>();
+    }
+}
