802aafdfa4e78a0e96908693ea5528cf566f05cf
skmkqw <tkorsakov77@gmail.com>
2026-02-20 22:16:22 +0100

    create dbcontext

---
+++ services/command/CommandService/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+using CommandService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandService.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Platform> Platforms { get; set; } = default!;
+    public DbSet<Command> Commands { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Platform>()
+            .HasMany(p => p.Commands)
+            .WithOne(p => p.Platform)
+            .HasForeignKey(p => p.PlatformId);
+
+        modelBuilder
+            .Entity<Command>()
+            .HasOne(p => p.Platform)
+            .WithMany(p => p.Commands)
+            .HasForeignKey(p => p.PlatformId);
+    }
+}
