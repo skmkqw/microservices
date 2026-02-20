4fbd51e55887dca67969533fd6bf25d661c4e154
skmkqw <tkorsakov77@gmail.com>
2026-02-20 22:16:15 +0100

    create models

---
+++ services/command/CommandService/Models/Platform.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.Models;
+
+public class Platform
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public int ExternalId { get; set; }
+
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    public ICollection<Command> Commands { get; set; } = new List<Command>();
+}
