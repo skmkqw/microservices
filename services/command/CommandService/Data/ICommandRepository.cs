282e19f93e53f4549ecd14d7c9e618e7c5b6f2c0
skmkqw <tkorsakov77@gmail.com>
2026-02-20 22:16:32 +0100

    create command repo

---
+++ services/command/CommandService/Data/ICommandRepository.cs
@@ -0,0 +1,18 @@
+using CommandService.Models;
+
+namespace CommandService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+
+    // Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool PlaformExits(int platformId);
+
+    // Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command? GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}
